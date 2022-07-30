c760806ee28c0b825c2cf0c5833f76c6b7da131a
Yancun Zhu <69443927+Relvyy@users.noreply.github.com>
2022-07-30 12:10:36 +0800

    Create test.py
    对预训练模型进行测试

---
+++ test.py
@@ -0,0 +1,18 @@
+from tools import *
+from model.model_zoo import *
+
+
+def test(model_string, r):
+    models = Model()
+    tool = Tools()
+    tool.try_gpu()
+
+    model = models.model(model_string),
+    model.initialize()
+    model.load_parameters(f'.\\runs\\{model_string}_best.params')
+    _, test_iter = tool.load_data_fashion_mnist(resize=r)
+    tool.test(model, test_iter, r)
+
+
+if __name__ == '__main__':
+    test('ResNet', 224)
